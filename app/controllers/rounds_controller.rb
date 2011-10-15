de92523e810d1de3756cad546bb5970f3a9bee67
Brett Bim <osiris43@gmail.com>
2011-10-14 19:50:07 -0500

    initial commit

---
+++ app/controllers/rounds_controller.rb
@@ -0,0 +1,5 @@
+class RoundsController < ApplicationController
+  def home
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twodowns::Application.config.secret_token = 'd48413317ae7b56f259faf6b0f4af837ca52f5ef66203453bb8bfa46b71f8e5c3dfbb66551ee4b717f26aa1c156c6de454c806197c1ac2b932db798dc91b850f'
